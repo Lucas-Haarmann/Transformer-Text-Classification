cce02608e3f7bb04ae8ef6c3e14c91294f31866e
Lucas-Haarmann <62995532+Lucas-Haarmann@users.noreply.github.com>
2022-05-15 04:25:33 -0700

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,10 +1,11 @@
 import numpy as np
 import torch
-from datasets import load_dataset
-from transformers import RobertaTokenizerFast
+from datasets import load_dataset, load_metric
+from transformers import LongformerTokenizer, LongformerForSequenceClassification, TrainingArguments, Trainer
 import labels
 
-MAX_SEQ_LEN = 20000
+MAX_SEQ_LEN = 4096
+NUM_LABELS = len(labels.idx_to_article)
 
 '''
 Convert list of facts into a single string
@@ -14,7 +15,7 @@ def combine_facts(example):
     combined_facts = ''
     for fact in example['facts']:
         combined_facts += fact[4:]
-    example['facts'] = combined_facts[:MAX_SEQ_LEN]
+    example['facts'] = combined_facts[-MAX_SEQ_LEN:]
 
     unclear = len(labels.idx_to_article) - 1
     label_list = []
@@ -30,15 +31,28 @@ def combine_facts(example):
     return example
 
 def main():
-    train_dataset = load_dataset('ecthr_cases', 'alleged-violation-prediction', split='train')
-    train_dataset = train_dataset.remove_columns(['silver_rationales', 'gold_rationales'])
-    train_dataset = train_dataset.map(combine_facts)
+    dataset = load_dataset('ecthr_cases', 'alleged-violation-prediction')
+    dataset = dataset.remove_columns(['silver_rationales', 'gold_rationales'])
+    dataset = dataset.map(combine_facts)
 
-    tokenizer = RobertaTokenizerFast.from_pretrained("roberta-base")
-    encoded_dataset = train_dataset.map(lambda examples: tokenizer(examples['facts'], padding=True), batched=True)
-    encoded_dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
-    dataloader = torch.utils.data.DataLoader(encoded_dataset, batch_size=32)
-    print(next(iter(dataloader)))
+    tokenizer = LongformerTokenizer.from_pretrained("allenai/longformer-base-4096")
+    dataset = dataset.map(lambda examples: tokenizer(examples['facts'], padding='max_length', max_length=MAX_SEQ_LEN))
+    dataset.set_format(type='torch', columns=['input_ids', 'attention_mask', 'labels'])
+    dataset = dataset.remove_columns('facts')
+
+    model = LongformerForSequenceClassification.from_pretrained("allenai/longformer-base-4096", num_labels=NUM_LABELS)
+    metric = load_metric('accuracy')
+    def eval_func(metric, output):
+        logits, labels = output
+        preds = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=preds, references=labels)
+    train_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch")
+    trainer = Trainer(model=model,
+                      args=train_args,
+                      train_dataset=dataset['train'],
+                      eval_dataset=dataset['validation'],
+                      compute_metrics=eval_func)
+    trainer.train()
 
 if __name__ == '__main__':
     main()
